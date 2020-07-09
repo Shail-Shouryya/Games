233259e9784027cfa1f0a2cbe83462ae0ac3f66a
Shail Shouryya <shailshouryya@gmail.com>
2020-07-08 20:46:24 -0700

    Add source for minesweeper game (python3)

---
+++ python/001_minesweeper.py
@@ -0,0 +1,5 @@
+'''
+This module was made using Leonard Yeo's tutorial on Medium from the following link:
+https://medium.com/swlh/this-is-how-to-create-a-simple-minesweeper-game-in-python-af02077a8de
+'''
+
